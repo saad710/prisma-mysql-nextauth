e9540a666b99564a7fa57b2a8f8f26163e0901eb
Md. Ifrat Hasnat <ifrathasnat@instawebworks.com.au>
2022-09-01 13:53:18 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -5,3 +5,7 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// DATABASE_URL = "mysql://root@localhost:3306/prisma_schema"
+// GOOGLE_CLIENT_ID=398291972603-m6jlft4scrjtqcugvdvbmggorj6ut7h1.apps.googleusercontent.com
+// GOOGLE_CLIENT_SECRET=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
