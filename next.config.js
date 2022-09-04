c1ed85702c66fe00617053a33558f7eef146b619
Md. Ifrat Hasnat <ifrathasnat@instawebworks.com.au>
2022-09-04 10:47:32 +0600

    database-connected-successfully

--- next.config.js
+++ next.config.js
@@ -9,3 +9,7 @@ module.exports = nextConfig
 // DATABASE_URL = "mysql://root@localhost:3306/prisma_schema"
 // GOOGLE_CLIENT_ID=398291972603-m6jlft4scrjtqcugvdvbmggorj6ut7h1.apps.googleusercontent.com
 // GOOGLE_CLIENT_SECRET=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
+
+// DATABASE_URL = "mysql://root@localhost:3306/prisma_schema"
+// GITHUB_ID=03f53cdc3cadab7e4c18
+// GITHUB_SECRET=c2a7cb3ecaf485a640d0bc72d406d088137aa038
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -8,9 +9,14 @@ const prisma = new PrismaClient()
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    GitHubProvider({
+      clientId:"03f53cdc3cadab7e4c18",
+      clientSecret:"c2a7cb3ecaf485a640d0bc72d406d088137aa038",
+    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId:"257707643962-cm74r2lm4hsr4h3lfgopfqmuogsl8ji7.apps.googleusercontent.com",
+      clientSecret:"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",
     }),
   ],
+
 })
